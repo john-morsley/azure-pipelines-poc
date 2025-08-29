24fc08a4aa4aa1ac113de52a13de32f4943eb3e8
John Morsley <johnmorsley@gmail.com>
2025-08-29 15:07:55 +0100

    A a passing unit test

---
+++ Morsley.UK.Tests.API.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Morsley.UK.Tests.API.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
